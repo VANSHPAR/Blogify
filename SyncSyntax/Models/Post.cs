121c2ba7de375835bc3ce15094c71e9de3ac7a85
Ahmed Qureshi <mr.ahmedQureshi022@outlook.com>
2025-02-22 00:42:16 +0500

    Add project files.

---
+++ SyncSyntax/Models/Post.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SyncSyntax.Models
+{
+    public class Post
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="The Title is Required")]
+        [MaxLength(400, ErrorMessage ="The title cannot exceed 400 characters")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "The Content is Required")]
+        public string Content { get; set; }
+
+        [Required(ErrorMessage = "The Author is Required")]
+        [MaxLength(100, ErrorMessage = "The Name cannot exceed 100 characters")]
+        public string Author { get; set; }
+
+        public string FeatureImagePath { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime PublishedDate { get; set; } = DateTime.Now;
+    }
+}
