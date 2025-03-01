42be0bb7a959cf01749d76a337dde482dea03495
Ahmed Qureshi <mr.ahmedQureshi022@outlook.com>
2025-03-01 19:25:51 +0500

    Added Controller

---
+++ SyncSyntax/Controllers/PostController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using SyncSyntax.Data;
+using SyncSyntax.Models.ViewModels;
+
+namespace SyncSyntax.Controllers
+{
+    public class PostController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public PostController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Create()
+        {
+            var postViewModel = new PostViewModel();
+            postViewModel.Categories = _context.Categories.Select(c =>
+                new SelectListItem
+                {
+                    Value = c.Id.ToString(),
+                    Text = c.Name
+                }
+            ).ToList();
+
+            return View(postViewModel);
+        }
+    }
+}
