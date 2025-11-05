776d3edd67ba1f92a26d33562c5f0844474dc3b4
Ahmed Qureshi <mr.ahmedQureshi022@outlook.com>
2025-11-06 02:24:50 +0500

    Part 25

---
+++ SyncSyntax/Controllers/AuthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SyncSyntax.Controllers
+{
+    public class AuthController : Controller
+    {
+        //Register
+        //Login
+        //Logout
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
---
+++ SyncSyntax/Models/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SyncSyntax.Models.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage ="Email is a Required Field")]
+        [EmailAddress(ErrorMessage ="Email must be in proper format")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Password is a Required Field")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Compare("Password",ErrorMessage ="Password must match the ConfirmPassword")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+    }
+}
