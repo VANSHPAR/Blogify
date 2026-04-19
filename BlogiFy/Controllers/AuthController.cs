4f9153f9bc3d8c84c33e349277ea0a0217e95363
Vansh Parmar <vanshpar6@gmail.com>
2026-04-19 19:20:11 +0530

    first commit

---
+++ BlogiFy/Controllers/AuthController.cs
@@ -0,0 +1,120 @@
+﻿using Azure.Core;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SyncSyntax.Models.ViewModels;
+
+namespace SyncSyntax.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        //Register
+        //Login
+        //Logout
+
+        public AuthController(UserManager<IdentityUser> userManager,
+            RoleManager<IdentityRole> roleManager,
+            SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _signInManager = signInManager;
+        }
+
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            //Check for validation
+            if (ModelState.IsValid)
+            {
+                //Create Identity User object 
+                var user = new IdentityUser { UserName = model.Email, Email = model.Email };
+
+                //User Create 
+                var result =  await _userManager.CreateAsync(user, model.Password);
+
+                //If User Ceated Successfully
+                if(result.Succeeded)
+                {
+                    //If the User Role exist in data base
+                    if(!await _roleManager.RoleExistsAsync("User"))
+                    {
+                       await _roleManager.CreateAsync(new IdentityRole("User"));
+                    }
+
+                   await _userManager.AddToRoleAsync(user, "User");
+                   await _signInManager.SignInAsync(user, isPersistent:true);
+
+                    return RedirectToAction("Index", "Post");
+                }
+
+            }
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByEmailAsync(model.Email);
+
+                if (user == null)
+                {
+                    ModelState.AddModelError("", "Email or Password is Incorrect");
+                    return View(model);
+                }
+
+               var signInresult =  await _signInManager.PasswordSignInAsync(user, model.Password, isPersistent:true, lockoutOnFailure:false);
+
+                if (!signInresult.Succeeded)
+                {
+                    ModelState.AddModelError("", "Email or Password is Incorrect");
+                    return View(model);
+                }
+
+                return RedirectToAction("Index", "Post");
+
+            }
+
+            return View(model);
+        }
+
+        [HttpGet]
+
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Post");
+        }
+
+        [HttpGet]
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
+    }
+}
+
+
+
+
+
---
+++ BlogiFy/Controllers/PostController.cs
@@ -0,0 +1,267 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SyncSyntax.Data;
+using SyncSyntax.Models;
+using SyncSyntax.Models.ViewModels;
+using System.Linq;
+
+namespace SyncSyntax.Controllers
+{
+   
+    public class PostController : Controller
+    {
+        private readonly AppDbContext _context;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        private readonly string[] _allowedExtension = {".jpg",".jpeg",".png" };
+        public PostController(AppDbContext context, IWebHostEnvironment webHostEnvironment)
+        {
+            _context = context;
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        [HttpGet]
+        public IActionResult Index(int? categoryId)
+        {
+            var postQuery = _context.Posts.Include(p => p.Category).AsQueryable();
+
+            if (categoryId.HasValue)
+            {
+                postQuery = postQuery.Where(p => p.CategoryId == categoryId);
+            }
+            var posts = postQuery.ToList();
+
+            ViewBag.Categories = _context.Categories.ToList();
+            return View(posts);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Detail(int id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            var post = _context.Posts.Include(p => p.Category).Include(p => p.Comments)
+                .FirstOrDefault(p=>p.Id == id);
+
+            if(post == null) 
+            {
+                return NotFound();
+            }
+            return View(post);
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "Admin")]
+        public IActionResult Create()
+        {
+            var postViewModel = new PostViewModel();
+            postViewModel.Categories = _context.Categories.Select(c =>
+                new SelectListItem
+                {
+                    Value = c.Id.ToString(),
+                    Text = c.Name
+                }
+            ).ToList();
+
+            return View(postViewModel);
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Create(PostViewModel postViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var inputFileExtension = Path.GetExtension(postViewModel.FeatureImage.FileName).ToLower();
+                bool isAllowed = _allowedExtension.Contains(inputFileExtension);
+
+                if (!isAllowed)
+                {
+                    ModelState.AddModelError("", "Invalid Image Format. Allowed Format are .jpg, .jpeg, .png");
+                    return View(postViewModel);
+                }
+
+                postViewModel.Post.FeatureImagePath = await UploadFiletoFolder(postViewModel.FeatureImage);
+                await _context.Posts.AddAsync(postViewModel.Post);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            postViewModel.Categories = _context.Categories.Select(c =>
+                new SelectListItem
+                {
+                    Value = c.Id.ToString(),
+                    Text = c.Name
+                }
+            ).ToList();
+
+            return View(postViewModel);
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Edit(int id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            var postFromDb = await _context.Posts.FirstOrDefaultAsync(p=>p.Id == id);
+            
+            if(postFromDb == null)
+            {
+                return NotFound();
+            }
+
+            EditViewModel editViewModel = new EditViewModel
+            {
+                Post = postFromDb,
+                Categories = _context.Categories.Select(c =>
+                new SelectListItem
+                {
+                    Value = c.Id.ToString(),
+                    Text = c.Name
+                }
+            ).ToList()
+            };
+
+            return View(editViewModel);
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Edit(EditViewModel editViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(editViewModel);
+            }
+
+            var postFromDb =await _context.Posts.AsNoTracking().FirstOrDefaultAsync(p=>p.Id==editViewModel.Post.Id);
+
+            if (postFromDb == null)
+            {
+                return NotFound();
+            }
+
+            if(editViewModel.FeatureImage != null)
+            {
+                var inputFileExtension = Path.GetExtension(editViewModel.FeatureImage.FileName).ToLower();
+                bool isAllowed = _allowedExtension.Contains(inputFileExtension);
+
+                if (!isAllowed)
+                {
+                    ModelState.AddModelError("", "Invalid Image Format. Allowed Format are .jpg, .jpeg, .png");
+                    return View(editViewModel);
+                }
+
+                var existingFilePath = Path.Combine(_webHostEnvironment.WebRootPath, "images", Path.GetFileName(postFromDb.FeatureImagePath));
+
+                if(System.IO.File.Exists(existingFilePath))
+                {
+                    System.IO.File.Delete(existingFilePath);
+                }
+
+                editViewModel.Post.FeatureImagePath = await UploadFiletoFolder(editViewModel.FeatureImage);
+            }
+            else
+            {
+                editViewModel.Post.FeatureImagePath = postFromDb.FeatureImagePath;
+            }
+            _context.Posts.Update(editViewModel.Post);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpGet]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var postFromDb = await _context.Posts.FirstOrDefaultAsync(p=>p.Id==id);
+            if (postFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(postFromDb);
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> DeleteConfirm(int id)
+        {
+            var postFromDb = await _context.Posts.FirstOrDefaultAsync(p => p.Id == id);
+            if (postFromDb == null)
+            {
+                return NotFound();
+            }
+
+            if (!string.IsNullOrEmpty(postFromDb.FeatureImagePath))
+            {
+                var existingFilePath = Path.Combine(_webHostEnvironment.WebRootPath, "images", Path.GetFileName(postFromDb.FeatureImagePath));
+
+                if (System.IO.File.Exists(existingFilePath))
+                {
+                    System.IO.File.Delete(existingFilePath);
+                }
+            }
+            _context.Posts.Remove(postFromDb);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Admin,User")]
+        public JsonResult AddComment([FromBody] Comment comment)
+        {
+            
+                comment.CommentDate = DateTime.Now;
+                _context.Comments.Add(comment);
+                _context.SaveChanges();
+
+                return Json(new
+                {
+                    userName = comment.UserName,
+                    commentDate = comment.CommentDate.ToString("MMMM dd, yyyy"),
+                    content = comment.Content
+                });
+            
+
+            
+        }
+
+        private async Task<string> UploadFiletoFolder(IFormFile file)
+        {
+            var inputFileExtension = Path.GetExtension(file.FileName);
+            var fileName = Guid.NewGuid().ToString() + inputFileExtension;
+            var wwwRootPath = _webHostEnvironment.WebRootPath;
+            var imagesFolderPath = Path.Combine(wwwRootPath, "images");
+
+            if (!Directory.Exists(imagesFolderPath))
+            {
+                Directory.CreateDirectory(imagesFolderPath);
+            }
+
+            var filePath = Path.Combine(imagesFolderPath,fileName);
+            try
+            {
+                await using(var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    await file.CopyToAsync(fileStream);
+                }
+            }
+            catch (Exception ex)
+            {
+                return "Error Uploading Images:" + ex.Message;
+            }
+
+            return "/images/" + fileName;
+
+        }
+    }
+}
